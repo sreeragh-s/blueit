7df0117333ddc1844c33033bd771e21f18d0715d
sreeragh <hey@sreeragh.me>
2025-03-29 22:58:59 +0530

    external commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import ThreadDetail from "./pages/ThreadDetail";
 import CommunityDetail from "./pages/CommunityDetail";
 import CreateThread from "./pages/CreateThread";
 import Login from "./pages/Login";
@@ -26,7 +25,6 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/thread/:threadId" element={<ThreadDetail />} />
             <Route path="/community/:communityId" element={<CommunityDetail />} />
             <Route path="/create" element={<CreateThread />} />
             <Route path="/login" element={<Login />} />
